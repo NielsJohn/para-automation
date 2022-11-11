23d08be585f5fe7f665580f6a81448a0552b3ea9
NielsJohn <nielsjohn90@gmx.de>
2022-11-11 10:21:13 +0100

    Add blank line to end of toml; add method para_path to dir_manipulation.py; INTERNAL_PATH as env variable in Makefile; add python-dotenv to requirements.txt; include env variables in settings.py; add method to remove folders to tasks.py

--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,28 @@
+import os
 from dataclasses import dataclass
 
 import toml
+from dotenv import load_dotenv
+
+
+class Environment:
+    def __init__(self) -> None:
+        load_dotenv()
+        self._internal_path = os.getenv("INTERNAL_PATH")
+
+    @property
+    def internal_path(self) -> str | None:
+        return self._internal_path
 
 
 @dataclass
 class Settings:
     """A class to store settings."""
 
+    env: Environment = Environment()
+
     def __post_init__(self) -> None:
-        with open('core/config.toml', 'r') as file:
+        with open("core/config.toml", "r") as file:
             self.toml_content = toml.load(file)
 
     @property
@@ -18,6 +32,10 @@ class Settings:
     @property
     def task_list(self) -> list[str]:
         return self.toml_content.get("app").get("task_list")
+
+    @property
+    def internal_path(self) -> str:
+        return self.env.internal_path
 
 
 settings = Settings()
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
-from core.tasks import task_factory
 from settings import settings
+from core.tasks import task_factory
+
 
 st.markdown("# Welcome to PARA!")
 
 # let the user select an action
 action = st.radio("What do you want to do?", settings.task_list)
-task = task_factory(action)
+try:
+    # get task from task factory
+    task = task_factory(action)
 
-# run the selected task
-task()
+    # run the selected task
+    task()
+except NotImplementedError:
+    st.markdown("### This method is not implemented (yet).")
